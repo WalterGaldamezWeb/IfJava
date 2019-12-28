78ff8801f042ad72a5c70f9bf0f6697ef242189c
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2019-12-27 18:33:59 -0600

    Uso de if en java

---
+++ src/ifjava/IfJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifjava;
+
+/**
+ *
+ * @author Walter Galdamez
+ */
+public class IfJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+    
+        if(a<20){
+            System.out.println("a es menor que 20");
+        }
+        //en el caso de que a sea mayor que 20 no imprime nada porque no se a declarado la sentencia else
+    }
+    
+}
